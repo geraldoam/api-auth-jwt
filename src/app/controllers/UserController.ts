aca1e480d61320b8342dd0cb65099da29e96b6fb
geraldoam <geraldoahnert@gmail.com>
2021-06-11 19:46:49 -0300

    fix: add return response json user

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -4,22 +4,22 @@ import { getRepository } from "typeorm";
 import User from "../models/User";
 
 class UserController {
-    async store(req: Request, res: Response) {
+    async store(request: Request, response: Response) {
         const repository = getRepository(User);
 
-        const { email, password } = req.body;
+        const { email, password } = request.body;
 
         const userExists = await repository.findOne({ where: { email } });
 
         if (userExists) {
-            return res.sendStatus(409); // Conflict
+            return response.sendStatus(409); // Conflict
         }
 
         const user = repository.create({ email, password });
 
         await repository.save(user);
 
-        return user;
+        return response.json(user);
     }
 }
 

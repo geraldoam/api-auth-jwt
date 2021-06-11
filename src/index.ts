c778043cd4f8a17aba5efcfb95f24cad79d97b3d
geraldoam <geraldoahnert@gmail.com>
2021-06-11 19:46:19 -0300

    fix: change port to 5000

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import "reflect-metadata";
 import express from "express";
+import "reflect-metadata";
 
 import "./database/connect";
 import routes from "./routes";
 
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => {
-    console.log("+ Server started at 3000.");
+app.listen(5000, () => {
+    console.log("+ Server started at 5000.");
 });

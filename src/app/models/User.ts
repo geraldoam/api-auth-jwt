5f58e7c7f51762263084310307ac7a6d315ebc9d
geraldoam <geraldoahnert@gmail.com>
2021-06-11 18:41:43 -0300

    feat: user model

---
+++ src/app/models/User.ts
@@ -0,0 +1,15 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity("users")
+class User {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+}
+
+export default User;

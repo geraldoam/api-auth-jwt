75add81137d43b1251eb8ea39d8e2e3e86708a6b
Geraldo Ahnert <geraldoahnert@gmail.com>
2021-09-03 12:47:08 -0300

    fix: env db name

--- ormconfig.js
+++ ormconfig.js
@@ -1,15 +1,15 @@
 require('dotenv/config');
 
 module.exports = {
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: 5432,
-    username: 'postgres',
-    password: process.env.DB_PASS,
-    database: 'tsauth',
-    entities: ['src/app/models/*.ts'],
-    migrations: ['src/database/migrations/*.ts'],
-    cli: {
-        migrationsDir: 'src/database/migrations/',
-    },
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: 'postgres',
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: ['src/app/models/*.ts'],
+  migrations: ['src/database/migrations/*.ts'],
+  cli: {
+    migrationsDir: 'src/database/migrations/',
+  },
 };
